dab88f9a7750b67e7b89db61f06f4752345f22d2
Faisal Jamil <faisaljamilcs@gmail.com>
2020-12-28 21:20:04 +0530

    feat: material ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,37 @@
-import '../styles/globals.css'
+import React from "react";
+// Modules
+import { AppProps } from "next/app";
+import Head from "next/head";
+// MUI Core
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
+// Utils
+import theme from "../utils/theme";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles && jssStyles.parentElement) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
 
-export default MyApp
+  return (
+    <>
+      <Head>
+        <title>My App</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default MyApp;
