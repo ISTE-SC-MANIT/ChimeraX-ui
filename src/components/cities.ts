b950dd9f548e08b5b073c7b79f96fbc12e3b1cb9
Akshay Pandey <imakshaypandey31@gmail.com>
2020-12-31 11:23:32 +0530

    add: new cities

--- src/components/cities.ts
+++ src/components/cities.ts
@@ -8,4 +8,119 @@ export const cities = [
     name: "Bhopal",
     state: "Madhya Pradesh",
   },
+
+  {
+    name: "Vadodara",
+    state: "Gujarat",
+  },
+
+  {
+    name: "Guwahati",
+    state: "Assam",
+  },
+
+  {
+    name: "Kolkata",
+    state: "West Bengal",
+  },
+
+  {
+    name: "Lucknow",
+    state: "Uttar Pradesh",
+  },
+
+  {
+    name: "Noida",
+    state: "Uttar Pradesh",
+  },
+
+  {
+    name: "Coimbatore",
+    state: "Tamil Nadu",
+  },
+
+  {
+    name: "Jammu",
+    state: "Jammu and Kashmir",
+  },
+
+  {
+    name: "Delhi",
+    state: "Delhi",
+  },
+
+  {
+    name: "Bangalore",
+    state: "Karnataka",
+  },
+
+  {
+    name: "Ahmedabad",
+    state: "Gujarat",
+  },
+
+  {
+    name: "Dehradun",
+    state: "Uttarakhand",
+  },
+
+  {
+    name: "Sagar",
+    state: "Madhya Pradesh",
+  },
+
+  {
+    name: "Indore",
+    state: "Madhya Pradesh",
+  },
+
+  {
+    name: "Gwalior",
+    state: "Madhya Pradesh",
+  },
+
+  {
+    name: "Kanpur",
+    state: "Uttar Pradesh",
+  },
+
+  {
+    name: "Nagpur",
+    state: "Maharashtra",
+  },
+
+  {
+    name: "Jaipur",
+    state: "Rajasthan",
+  },
+
+  {
+    name: "Udaipur",
+    state: "Rajasthan",
+  },
+
+  {
+    name: "Chandigarh",
+    state: "Punjab",
+  },
+
+  {
+    name: "Ujjain",
+    state: "Madhya Pradesh",
+  },
+
+  {
+    name: "Jhansi",
+    state: "Uttar Pradesh",
+  },
+
+  {
+    name: "Chennai",
+    state: "Tamil Nadu",
+  },
+
+  {
+    name: "Raipur",
+    state: "Chhattisgarh",
+  },
 ];
