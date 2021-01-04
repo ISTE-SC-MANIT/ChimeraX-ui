2764c390218ff350517eaf29b37912fcbd75eb52
AyushRusia <ayushrusia11@outlook.com>
2021-01-04 13:35:09 +0530

    feat:flow reset password

--- src/components/forgotPassword.tsx
+++ src/components/forgotPassword.tsx
@@ -6,6 +6,11 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import { Formik, Form, Field, FieldProps } from 'formik';
+import * as yup from 'yup';
+import axios from 'axios';
+import { authenticate } from '../components/utils';
+import { useRouter } from 'next/router';
 
 interface Props {
     open: boolean,
@@ -14,7 +19,27 @@ interface Props {
 
 const FormDialog: React.FC<Props> = ({ open, onClose }) => {
     // const [open, setOpen] = React.useState(false);
+    const [formData, setFormData] = React.useState({ email: ''});
+    const router = useRouter();
 
+        const initialValues = {
+        email: '',
+        };
+      
+     const handleSubmit = (values:typeof initialValues) => {
+       setFormData({ ...formData });
+
+       axios
+         .put(`${process.env.NEXT_PUBLIC_BACKEND}/api/forgotpassword`, { ...values })
+         .then((response) => {
+           authenticate(response, () => {
+             console.log('working');
+           });
+         })
+         .catch((error) => {
+           return error;
+         });
+     }
     const handleClickOpen = () => {
         onClose()
     };
@@ -23,34 +48,56 @@ const FormDialog: React.FC<Props> = ({ open, onClose }) => {
         onClose()
     };
 
-    return (
-        <div>
+    
 
-            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-                <DialogTitle id="form-dialog-title"> Forgot Password ?</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        Enter your email id , a reset link will be sent to your registered email id which will redirect you to update pass word page.
-          </DialogContentText>
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="name"
-                        label="Email Address"
-                        type="email"
-                        fullWidth
-                    />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose} color="primary">
-                        Cancel
-          </Button>
-                    <Button onClick={handleClose} color="primary">
-                        Submit
-          </Button>
-                </DialogActions>
-            </Dialog>
-        </div>
+    return (
+      <div>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title"> Forgot Password ?</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Enter your email id , a reset link will be sent to your registered email id which will
+              redirect you to update pass word page.
+            </DialogContentText>
+
+             <Formik
+                onSubmit={(values) => handleSubmit(values)}
+                initialValues={initialValues}
+            >
+              <Form id="myform">
+                <Field name="email">
+            {({ field, meta }: FieldProps<typeof initialValues['email']>) => 
+            ( <TextField
+                  autoFocus
+                  margin="dense"
+                  id="email"
+                  label="Email Address"
+                  type="email"
+                  fullWidth
+                />)}
+                </Field>
+             
+
+        
+          
+            <Button onClick={handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button type="submit" color="primary">
+              Submit
+            </Button>
+         
+             </Form>
+            </Formik>
+         
+            </DialogContent>
+        </Dialog>
+      </div>
     );
 }
 
